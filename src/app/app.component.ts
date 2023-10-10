a194d41f975e84f3da63a786ea17d7f39356fee6
Giannnnnn <giremio5@gmail.com>
2023-10-10 17:13:28 -0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private _formBuilder: FormBuilder) {}
+
+  firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
+  secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
+  thirdFormGroup: FormGroup = this._formBuilder.group({thirdCtrl: ['']});
+  
   title = 'NeomotLPC';
+  gola: string = '';
+  aberturaPorta: string = '';
+  qdComando: string = '';
+  medida: string = '';
+  casaDeMaquina: string = '';
+
+  golas: string[] = ['Alvenaria', 'Concreto', 'Bloco est'];
+  aberturaPortas: string[] = ['Lateral D.', 'Lateral esq.', 'Central'];
+  qdComandos: string[] = ['Porta pav.', 'Alçapão', 'Outro'];
+  medidas: string[] = ['Acabadas', 'Em Bruto'];
+  casaDeMaquinas: string[] = ['Possui', 'Não possui'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatRadioModule } from '@angular/material/radio';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgFor } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatStepperModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    FormsModule,
+    NgFor,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
